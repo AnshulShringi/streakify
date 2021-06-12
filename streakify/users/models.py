f01b8b7c2e7599bae586ffa4d16546f1a93bd5d9
Anshul Shringi <shringiansh@gmail.com>
2021-06-12 13:12:13 +0530

    streak app models

--- streakify/users/models.py
+++ streakify/users/models.py
@@ -1,7 +1,6 @@
 # Django imports
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.validators import UnicodeUsernameValidator
 
@@ -37,9 +36,6 @@ class User(AbstractUser):
 
     class Meta:
         unique_together = ('country_code', 'mobile_number',)
-    
-    def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
 
     def __str__(self):
         return self.phone
