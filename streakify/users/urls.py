05848638caf3e9cf194f1d124444c104d04ed6d8
Anshul Shringi <shringiansh@gmail.com>
2021-06-12 09:21:25 +0530

    deleted unnecessary files

--- streakify/users/urls.py
+++ streakify/users/urls.py
@@ -10,8 +10,4 @@ app_name = "users"
 urlpatterns = [
     path("get-token/", GetTokenView.as_view(), name='token_obtain'),
     path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
-    path("update-user-profile/", view=UpdateUserProfile.as_view(), name="update-user-profile"),
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
 ]
