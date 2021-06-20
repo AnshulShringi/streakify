aeb18365bc7d3ace8aa40e3e0307ef7f34463c0a
Anshul Shringi <shringiansh@gmail.com>
2021-06-20 14:56:30 +0530

    user profile pic

--- streakify/users/serializers.py
+++ streakify/users/serializers.py
@@ -3,34 +3,34 @@ from streakify.users.models import User, UserProfile
 
 
 class UserSerializer(serializers.ModelSerializer):
-    country_code = serializers.SerializerMethodField()
-    mobile_number = serializers.SerializerMethodField()
+    country_code = serializers.ReadOnlyField(source='user_profile.country.country_code')
+    mobile_number = serializers.ReadOnlyField(source='user_profile.mobile_number')
     profile_pic = serializers.SerializerMethodField()
     
     class Meta:
         model = User
         fields = ['id', 'name', 'email', 'country_code', 'mobile_number', 'profile_pic']
 
-    def get_country_code(self, obj):
-        profile = UserProfile.objects.filter(user=obj)
-        if profile:
-            return profile[0].country.country_code
-        return ""
-
-    def get_mobile_number(self, obj):
-        profile = UserProfile.objects.filter(user=obj)
-        if profile:
-            return profile[0].mobile_number
-        return ""
-
     def get_profile_pic(self, obj):
-        profile = UserProfile.objects.filter(user=obj)
-        if profile and profile[0].profile_pic:
-            return profile[0].profile_pic
+        if obj.user_profile.profile_pic:
+            return obj.user_profile.profile_pic.url
         return ""
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
+    profile_pic = serializers.FileField(required=False)
+    
     class Meta:
         model = User
-        fields = [ 'name', 'email' ]
+        fields = [ 'name', 'email', 'profile_pic' ]
+
+    def update(self, instance, validated_data):
+        profile_pic = validated_data.pop('profile_pic') if 'profile_pic' in validated_data else None
+        instance.email = validated_data.get('email', instance.email)
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        if profile_pic:
+            profile = instance.user_profile
+            profile.profile_pic = profile_pic
+            profile.save()
+        return instance
